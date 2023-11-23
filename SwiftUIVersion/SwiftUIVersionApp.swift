ca6bb43d3c3db029581e9f2d9437c09ab6296997
yeomsungpil <present7256@gmail.com>
2023-11-23 10:11:45 +0900

    Initial Commit

---
+++ SwiftUIVersion/SwiftUIVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVersionApp.swift
+//  SwiftUIVersion
+//
+//  Created by 염성필 on 2023/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
