ecd0c7ace88f064ca3dbb13cfa972ada133404cc
yeomsungpil <present7256@gmail.com>
2023-11-23 11:36:27 +0900

    TabView - TabItem 추상화 시키기 + Enum

--- SwiftUIVersion/TabBadgeView.swift
+++ SwiftUIVersion/TabBadgeView.swift
@@ -10,29 +10,66 @@ import SwiftUI
 struct TabBadgeView: View {
     var body: some View {
         TabView {
-            ZStack {
-                Color.red
-                Text("1")
+            ForEach(TabItem.allCases, id:\.hashValue) { item in
+                item.rootView
                     .tabItem {
-                        Label("홈", systemImage: "house")
+                        item.image
                     }
-                .badge(30)
-            } // 15+
-            ContentView()
-                .tabItem {
-                    Label("리스트", systemImage: "doc.fill")
-                }
-                // Custom modifier를 사용하여 Version에 따른 분기 처리하기
-                .badges(20)
-            Text("3")
-                .tabItem {
-                    Label("마이페이지", systemImage: "person")
-                }
-                .badges(5)
+            }
         }
         // tabViewStyle에 따라 pageControl(Label이 없다면 기본형인 O O O 이렇게 나옴)
-        .tabViewStyle(.page)
+//        .tabViewStyle(.page)
     }
+}
+
+extension TabBadgeView {
+    // 열거형으로 구분 - 현재 파일 내에서만 사용하기 때문에 접근 제어자 설정
+    private enum TabItem: CaseIterable {
+        case home
+        case favorite
+        case list
+        case setting
+        
+        var image: Image {
+            switch self {
+            case .home:
+                return Image(systemName: "house")
+            case .favorite:
+                return Image(systemName: "star")
+            case .list:
+                return Image(systemName: "doc.fill")
+            case .setting:
+                return Image(systemName: "gear")
+            }
+        }
+        
+        @ViewBuilder // body가 아닌곳에서 View에 직접적으로 UI를 그릴때는 ViewBuilder를 사용함
+        var rootView: some View {
+            switch self {
+            case .home:
+                ContentView()
+            case .favorite:
+                ZStack {
+                    Color.red
+                    Text("ROOT VIEW")
+                }
+            case .list:
+                ZStack {
+                    Color.yellow
+                    Text("ROOT VIEW")
+                }
+            case .setting:
+                ZStack {
+                    Color.gray
+                    Text("ROOT VIEW")
+                }
+            }
+           
+        }
+        
+        
+    }
+    
 }
 
 struct TabBadgeView_Previews: PreviewProvider {
